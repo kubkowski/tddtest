21b6910b762cf35ea3606f624c2690596aa9d17e
kubkowski <jakub.opara@gmail.com>
2016-06-30 22:37:36 +0200

    refactor templates to use a base template

--- superlists/urls.py
+++ superlists/urls.py
@@ -21,6 +21,5 @@ from lists import urls as list_urls
 urlpatterns = [
 #    url(r'^admin/', include(admin.site.urls)),
     url(r'^$', list_views.home_page, name='home'),
-    url(r'^lists/$', include(list_urls)),
-
+    url(r'^lists/', include(list_urls)),
 ]
